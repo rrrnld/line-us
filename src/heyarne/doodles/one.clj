e89440a054a240fe1036d9ecd709c8b02ed38d98
heyarne <arne@schlueter.is>
2019-10-27 15:32:15 +0100

    Finish first doodle

---
+++ src/heyarne/doodles/one.clj
@@ -0,0 +1,49 @@
+(ns heyarne.doodles.one
+  (:require [clojure2d.core :as c2d]
+            [clojure2d.color :as color]
+            [infix.macros :refer [infix]]
+            [thi.ng.geom.line :as l]))
+
+(defn lerp [v min-v max-v min-t max-t]
+  (+ min-t (* (- max-t min-t) (/ (- v min-v) (- max-v min-v)))))
+
+(defn setup [canvas]
+  (let [gap 10
+        segment-padding (* 25 gap)]
+    {:lines (for [y (range gap (inc (- (:h canvas) gap)) gap)
+                  :let [perc (/ (- y gap) (- (:h canvas) (* 2 gap)))
+                        cos (Math/cos (* Math/PI 2 perc))
+                        segment-x (lerp cos -1 1
+                                        (* 0.5 segment-padding)
+                                        (- (:w canvas) (* 0.5 segment-padding)))
+                        segment-y (+ y (lerp cos -1 1 40 0))]]
+              (l/linestrip2 [gap y] [segment-x segment-y] [(- (:w canvas) gap) y]))}))
+
+
+(defn draw-state [canvas state]
+  (c2d/set-color canvas :white)
+  (c2d/rect canvas 0 0 (:w canvas) (:h canvas))
+  (c2d/set-color canvas :salmon)
+  (doseq [line (:lines state)]
+    (let [[start segment end] (:points line)]
+      (c2d/line canvas start segment)
+      (c2d/line canvas segment end))))
+
+(defn update-state [canvas state]
+  state)
+
+(defn print! [sketch]
+  (let [state (c2d/get-state sketch)]))
+
+(defn -main [& args]
+  (let [canvas (c2d/canvas 400 400)]
+    (c2d/show-window
+     {:window-name "Doodle One"
+      :canvas canvas
+      :draw-state (setup canvas)
+      :draw-fn (fn [canvas _window _frame state]
+                 (draw-state canvas state)
+                 (update-state canvas state))})))
+
+(comment
+  (def sketch (-main)))
