682ec919646a80f97381c1d66b89f384d796165e
heyarne <arne@schlueter.is>
2019-10-28 23:53:04 +0100

    Add some comments to doodle one and implement plotting functions

--- src/heyarne/line_us/doodles/one.clj
+++ src/heyarne/line_us/doodles/one.clj
@@ -1,12 +1,18 @@
 (ns heyarne.line-us.doodles.one
-  (:require [clojure2d.core :as c2d]
-            [thi.ng.geom.line :as l]))
+  (:require [heyarne.line-us.connection :as line-us]
+            [clojure2d.core :as c2d]
+            [thi.ng.geom.core :as g]
+            [thi.ng.geom.line :as l]
+            [thi.ng.geom.polygon :as p]
+            [thi.ng.geom.rect :as r]))
 
-(defn lerp [v min-v max-v min-t max-t]
+(defn lerp
+  "Normalizes a value from [min-v, max-v] to [min-t, max-t]"
+  [v min-v max-v min-t max-t]
   (+ min-t (* (- max-t min-t) (/ (- v min-v) (- max-v min-v)))))
 
 (defn setup [canvas]
-  (let [gap 10
+  (let [gap 5
         segment-padding (* 25 gap)]
     {:lines (for [y (range gap (inc (- (:h canvas) gap)) gap)
                   :let [perc (/ (- y gap) (- (:h canvas) (* 2 gap)))
@@ -29,6 +35,46 @@
 (defn update-state [canvas state]
   state)
 
+(defn state->g01
+  "Converts the state to plotting coordinates; scales the coordinates to fill
+  the safe drawing area"
+  [state]
+  ;; FIXME: At the moment this doesn't preserve aspect ratio
+  (let [bounds (->> (:lines state)
+                    (mapcat :points)
+                    (p/convex-hull*)
+                    (p/polygon2)
+                    (g/bounds))
+        [min-x min-y] (r/bottom-left bounds)
+        [max-x max-y] (r/top-right bounds)
+        [left right] (:x line-us/drawing-area)
+        [down up] (:y line-us/drawing-area)]
+    (->> (:lines state)
+         (map :points)
+         ;; scale points to fill the whole drawing area
+         (map (fn [points]
+                (map #(-> (update % 0 lerp min-x max-x left right)
+                          (update 1 lerp min-y max-y down up)) points)))
+         ;; make sure the plotter touches the paper
+         (map (fn [points]
+                (mapv #(conj % 200) points)))
+         ;; lift the plotter after each line and join the movements
+         (mapcat (fn [points]
+                   (conj points (assoc (last points) 2 1000)))))))
+
+(defn plot!
+  "Sends the current global state of a sketch to the line-us for drawing"
+  [sketch]
+  (let [movements (state->g01 (c2d/get-state sketch))
+        println (partial println "Line-us:")]
+    (with-open [line-us (line-us/connect "line-us.lan" 1337)]
+      (println (line-us/read-response line-us))
+      (Thread/sleep 1000)
+      (doseq [coords movements]
+        (println (line-us/send-movement! line-us coords)))
+      (Thread/sleep 1000)
+      (println "Done!"))))
+
 (defn -main [& args]
   (let [canvas (c2d/canvas 400 400)]
     (c2d/show-window
@@ -41,4 +87,7 @@
                    (update-state canvas state)))})))
 
 (comment
-  (def sketch (-main)))
+  ;; to run the sketch, evaluate this line:
+  (def sketch (-main))
+  ;; to start plotting the sketch:
+  (plot! sketch))
